11b58b5c703444f133eeec4aa5cb39e6b5831c56
Daniel Johansen <drdj@hotmail.dk>
2024-10-03 22:33:45 +0200

    Initial commit

---
+++ email/SaveEmail.php
@@ -0,0 +1,22 @@
+<?php
+// Get the email from the POST request
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+    
+    // Sanitize the email input
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    
+    // Specify the file to store emails
+    $file = 'email/emails.csv';
+    
+    // Append the email to the CSV file
+    $handle = fopen($file, 'a');
+    fputcsv($handle, [$email]);
+    fclose($handle);
+    
+    // Send a response back to the AJAX call
+    echo 'Email saved successfully!';
+} else {
+    echo 'Invalid request!';
+}
+?>
